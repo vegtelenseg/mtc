81192c78fdaa2e5ee24b652422a732dff6b09244
Siya <dot.netntoz@gmail.com>
2018-07-24 11:59:35 +0200

    initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../header/header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+      <Header/>
+      </div>
+    );
+  }
+}
+
